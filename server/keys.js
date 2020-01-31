5c320a753fbb3b55ecdb2bf654c02bec6ca49768
Joshua Kingswell <joshua.kingswell@rradar.com>
2020-01-31 16:09:01 +0000

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  phDatabase: process.env.PGDATABASE,
+  phPassword: process.env.PGPASSWORD,
+  pgPORT: process.env.PGPORT
+};
